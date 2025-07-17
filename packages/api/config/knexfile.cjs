a0c22d63e2d2d1ad0d7dbc55cdd59090c4e7fa8a
Milad Arbabi <arbabi.milad@gmail.com>
2025-07-17 19:05:36 +0200

    setting up the ci CLI commands for github actions

--- packages/api/config/knexfile.cjs
+++ packages/api/config/knexfile.cjs
@@ -11,5 +11,16 @@ module.exports = {
     },
     migrations: { directory: '../src/db/migrations', extension: 'cjs' },
     seeds:      { directory: './seeds',      extension: 'cjs' }
-  }
+  },
+  test: {
+       client: 'pg',
+       connection: {
+         host:     process.env.PGHOST     || '127.0.0.1',
+         user:     process.env.PGUSER     || 'postgres',
+         password: process.env.PGPASSWORD || 'postgres',
+         database: process.env.PGDATABASE || 'winlo_test'
+       },
+       migrations: { directory: '../src/db/migrations', extension: 'cjs' },
+       seeds:      { directory: '../src/db/seeds',      extension: 'cjs' }
+     }
 };
